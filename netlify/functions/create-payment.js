7ec15a7600dfc10da04d0b5ff6f553cf8a929b68
uhdghdj <jrpoyka@gmail.com>
2026-04-25 10:56:10 +0200

    Update create-payment.js

--- netlify/functions/create-payment.js
+++ netlify/functions/create-payment.js
@@ -1,11 +1,12 @@
+
 export const handler = async (event) => {
   try {
     const body = JSON.parse(event.body || "{}");
 
     const CONFIG = {
-      card: { integration_id:5245183 , iframe_id: 952326  },
-      wallet: { integration_id:5245282 , iframe_id:972789  },
-    
+      card: { integration_id: 5369099, iframe_id: 972116 },
+      wallet: { integration_id: 5371332, iframe_id: 972117 },
+      kiosk: { integration_id: 5369208 },
     };
 
     const integrationType = body.integration_type || "card";
@@ -29,8 +30,15 @@ export const handler = async (event) => {
 
     const amount_cents = Math.round(amount * 100);
 
-    // 🔥 API KEY محطوط مباشرة (للتجربة فقط)
-    const API_KEY = "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TVRBM01EYzRPU3dpYm1GdFpTSTZJakUzTmpBeE9UUXlNamN1TXpBNU1UZ3pJbjAuODdYUkRfenRZSWp6YkhrbWZvLXlpMmh2dDZlZEloMzBwSjctUE9GSkItRzdVMUc1NzhBeVRacGFfVXI3VHVlRnZ4VDYxSklxUDFTQzBSV2N4eHRKcHc=";
+    // 🔥 API KEY (هنا تحطه)
+    const API_KEY = process.env.PAYMOB_API_KEY;
+
+    if (!API_KEY) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "API KEY مش موجود في env" }),
+      };
+    }
 
     // 1️⃣ Auth
     const authRes = await fetch("https://accept.paymob.com/api/auth/tokens", {
@@ -59,7 +67,6 @@ export const handler = async (event) => {
     );
 
     const orderData = await orderRes.json();
-    if (!orderData.id) throw new Error("Order failed");
 
     // 3️⃣ Payment Key
     const payRes = await fetch(
@@ -94,6 +101,7 @@ export const handler = async (event) => {
     );
 
     const payData = await payRes.json();
+
     if (!payData.token) throw new Error("Payment key فشل");
 
     // 4️⃣ Kiosk
@@ -141,4 +149,4 @@ export const handler = async (event) => {
       body: JSON.stringify({ error: err.message }),
     };
   }
-};
+};      
